65667ee2ff322ace4676a8bde726a710ae4def12
misa <misael.barria@hotmail.com>
2016-11-08 23:05:30 -0300

    Agregado logro base en seeds y logica al controlador de logros

--- app/controllers/logros_controller.rb
+++ app/controllers/logros_controller.rb
@@ -12,17 +12,41 @@ class LogrosController < ApplicationController
 
   def destroy
   	 @logro=Logro.find(params[:id])
-  	 @logro.destroy
-  	 redirect_to logros_path, notice: 'Logro Eliminado'
+  	 if @logro.id == 1 then
+  	 	redirect_to logros_path, notice: 'No se puede eliminar este logro'
+  	 else
+  	 	@logro.destroy
+  	 	redirect_to logros_path, notice: 'Logro Eliminado'
+  	 end
   end
   
   def create
-  	 @l=Logro.create(etiqueta: params[:logro][:etiqueta], puntosMin: params[:logro][:puntosMin], puntosMax: params[:logro][:puntosMax])
- 	 redirect_to logros_path, notice: 'Logro Creado'
+  	@l=Logro.new(etiqueta: params[:logro][:etiqueta], puntosMin: params[:logro][:puntosMin], puntosMax: params[:logro][:puntosMax])
+  	if(@l.puntosMin >= @l.puntosMax) then
+  		return redirect_to new_logro_path, notice: 'Rango de puntos invalido'
+  	end
+  	logros = Logro.all
+	logros.each do |l|
+  	 if( l.etiqueta == @l.etiqueta ) then
+  	 	return redirect_to logros_path, notice: 'Logro existente'
+  	 end
+  	end
+  	@l.save
+ 	redirect_to logros_path, notice: 'Logro Creado'
   end
 
   def update
   	@logro=Logro.find(params[:id])
+  	@l=Logro.new(etiqueta: params[:logro][:etiqueta], puntosMin: params[:logro][:puntosMin], puntosMax: params[:logro][:puntosMax])
+  	if(@l.puntosMin >= @l.puntosMax) then
+  		return redirect_to edit_logro_path(@logro), notice: 'Rango de puntos invalido'
+  	end
+  	logros = Logro.all
+	logros.each do |l|
+  	 if( l.etiqueta == @l.etiqueta ) then
+  	 	return redirect_to edit_logro_path(@logro), notice: 'Logro existente'
+  	 end
+  	end
   	@logro.update(etiqueta: params[:logro][:etiqueta], puntosMin: params[:logro][:puntosMin], puntosMax: params[:logro][:puntosMax])
   	redirect_to logros_path, notice: 'Logro Actualizado'
   end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+puts "Creo logro base"
+Logro.find_or_create_by(id: 1, etiqueta: "Newbie", puntosMin: 0, puntosMax: 5)
