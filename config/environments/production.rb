5a25b899e30c96795702566349f59e0b61bf9339
macuduranti <macuduranti@gmail.com>
2016-11-16 19:08:50 -0300

    cambio config mailer heroku/test

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -81,12 +81,12 @@ Rails.application.configure do
 
   Rails.application.routes.default_url_options[:host] = 'unagauchada-mh.herokuapp.com'
 
-  config.action_mailer.delivery_method = :smtp
+  ActionMailer::Base.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = false
   config.action_mailer.default :charset => "utf-8"
 
-  config.action_mailer.smtp_settings = {
+ ActionMailer::Base.smtp_settings = {
     address: "smtp.gmail.com",
     port: 587,
     domain: ENV["GMAIL_DOMAIN"],
